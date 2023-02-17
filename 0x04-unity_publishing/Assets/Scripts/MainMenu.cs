485e5f7a57fa90a72d5e15625537c2887870156f
AnActualBanana <loganmcclain59@gmail.com>
2023-02-17 13:39:39 -0600

    builds

--- 0x04-unity_publishing/Assets/Scripts/MainMenu.cs
+++ 0x04-unity_publishing/Assets/Scripts/MainMenu.cs
@@ -11,12 +11,18 @@ public class MainMenu : MonoBehaviour
 	public Toggle colorblindMode;
 	public void PlayMaze()
 	{
-		SceneManager.LoadScene("maze");
-		 if (colorblindMode != null && colorblindMode.isOn)
+		if (colorblindMode != null && colorblindMode.isOn)
     	{
-        trapMat.color = new Color32(255, 112, 0, 1);
-        goalMat.color = Color.blue;
+			trapMat.color = new Color32(255, 112, 0, 1);
+			goalMat.color = Color.blue;
     	}
+		else if (!colorblindMode.isOn)
+		{
+			trapMat.color = Color.red;
+			goalMat.color = Color.green;
+		}
+		SceneManager.LoadScene("maze");
+
 	}
 
     public void QuitMaze()
